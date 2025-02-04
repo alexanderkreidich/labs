e0ed22681bbb06260db3c91b4a5aeafabbd8483f
sasha <kredich05@mail.ru>
2025-02-04 21:03:07 +0300

    lab6.1 and .gitignore

--- lab6.1.cpp
+++ lab6.1.cpp
@@ -2,36 +2,36 @@
 #include <stack>
 #include <string>
 
-using namespace ltd;
+using namespace std;
 
 bool stackCheck(const string& exp ) {
     stack<char> s; // stack
 
-    for (ch : exp) {
+    for (char ch : exp) {
 
-        if (ch == "(" || ch == "{" || ch == "[") {
+        if (ch == '(' || ch == '{' || ch == '[') {
 
-            s.push(ch);
+          s.push(ch);
 
         }
         
-        else if (ch == ")" || ch == "}" || ch == "]") {
+        else if (ch == ')' || ch == '}' || ch == ']') {
 
             if (s.empty()) {
 
-                return false; //стек пуст но есть закрыв скобка
+              return false; //стек пуст но есть закрыв скобка
 
             }
         
             char top = s.top(); // вверхний стек
 
-            if ((top + ch == "()") || (top + ch == "[]") || (top + ch == "{}")) {
+            if ((top == '(' && ch == ')') || (top == '[' && ch == ']') || (top == '{' && ch == '}')) {
 
-                s.pop(); // пара скобок найдена
+              s.pop(); // пара скобок найдена
 
             }
             else {
-                return false;
+              return false;
             }
         }
     }
@@ -42,10 +42,27 @@ bool stackCheck(const string& exp ) {
 
 int main() {
     string exp1 = "2+4-5(87192479_7348){72648]76823[]";
-    cout << exp1 + " -> " + stackCheck(exp1);
+    if (stackCheck(exp1) == true) {
+      cout << exp1 + " -> " + " yes\n";
+    }
+    else {
+      cout << exp1 + " -> " + " no\n";
+    }
+
     string exp2 = "({})";
-    cout << exp2 + " -> " + stackCheck(exp2);
+    if (stackCheck(exp2) == true) {
+      cout << exp2 + " -> " + " yes\n";
+    }
+    else {
+      cout << exp2 + " -> " + " no\n";
+    }
+
     string exp3 = "({)}";
-    cout << exp3 + " -> " + stackCheck(exp3);
+    if (stackCheck(exp3) == true) {
+      cout << exp3 + " -> " + " yes\n";
+    }
+    else {
+      cout << exp3 + " -> " + " no\n";
+    }
 
 }
